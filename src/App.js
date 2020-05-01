f416b717583281d6865319efd0ccebbe811968e7
sunilshrestha123 <ilamalisunilshrestha00@gmail.com>
2020-05-01 03:41:37 -0700

    new deployment git hub pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Cardlist from './components/card-list/card-list.components';
+import SearchBox from './components/search-box/search-box';
+import Hello from './page/hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nameDetails: [],
+      searchfield: '',
+      // name: 'sunil shrestha',
+      // age: 12,
+      // text: 'helllo nepali ',
+      // status: true,
+    };
+    // this.onChange = this.onChange.bind(this);
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((user) => this.setState({ nameDetails: user }));
+  }
+  onChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+  ///rsponse.json in the method which return jason data javascript readable formate
+  render() {
+    const { nameDetails, searchfield } = this.state;
+    const filternameDetails = nameDetails.filter((details) =>
+      details.name.toLowerCase().includes(searchfield.toLowerCase())
+    );
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <SearchBox
+            placeholder='please enter the name '
+            onChange={this.onChange}
+          />
+          <Hello />
+          {/* <p>
+            {this.state.nameDetails.map((namedetails) => (
+              <h1>
+                {namedetails.name}
+                {namedetails.age}
+              </h1>
+            ))}
+          </p>
+
+          <p>{name}</p>
+          <p>{age}</p>
+          <p>{status}</p> */}
+
+          {/* <p>{this.state.age}</p> */}
+
+          {/* <p onMouseOverCapture={() => this.setState({ text: 'fuck ' })}>
+            {this.state.text}
+          </p> */}
+
+          {/* <button onClick={() => this.setState({ name: 'mina tamang' })}>
+            click me to change name
+          </button> */}
+          {/* <h1>{filtername.name}</h1> */}
+
+          <Cardlist monsters={filternameDetails}>mina tamang</Cardlist>
+          <div></div>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
