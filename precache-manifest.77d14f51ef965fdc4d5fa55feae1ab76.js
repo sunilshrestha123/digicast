096c2e61d5894abb73770b5f5008e32f642818e2
sunilshrestha123 <ilamalisunilshrestha00@gmail.com>
2020-05-01 03:53:52 -0700

    Updates

---
+++ precache-manifest.77d14f51ef965fdc4d5fa55feae1ab76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b7c012fa0ec5e6e39039fa8a881c8d",
+    "url": "/digicast/index.html"
+  },
+  {
+    "revision": "e56f206a3436903a1e3e",
+    "url": "/digicast/static/css/main.6276da58.chunk.css"
+  },
+  {
+    "revision": "8ad8f322054779c2d0b0",
+    "url": "/digicast/static/js/2.9e198111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digicast/static/js/2.9e198111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56f206a3436903a1e3e",
+    "url": "/digicast/static/js/main.806f4b7d.chunk.js"
+  },
+  {
+    "revision": "12c3f8f496b9ae8d31b2",
+    "url": "/digicast/static/js/runtime-main.c4512c62.js"
+  }
+]);
